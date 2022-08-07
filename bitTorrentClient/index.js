9417288460fb1b88eb7d256a826866799cf7872a
neoZorba <amaltathva@gmail.com>
2022-08-07 15:00:43 +0530

    done connecting client and tracker

--- bitTorrentClient/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const bencode = require('bencode');
-const tracker = require('./tracker');
-
-const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-
-tracker.peersList(torrent, (peers) => {
-  console.log('list of peers: ', peers);
-});
