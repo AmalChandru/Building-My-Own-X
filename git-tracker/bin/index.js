3c5d5fe121a843107e39dd0f3c5c02649068c826
Amal Chandran M V <amaltathva@gmail.com>
2023-02-18 12:07:12 +0530

    feat: print git enabled folders inside a directory

--- git-tracker/bin/index.js
+++ git-tracker/bin/index.js
@@ -1,29 +1,41 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 const fs = require('fs');
-const { spawnSync } = require('child_process');
+const path = require('node:path');
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
+
+function doGit(_directory, _command) {
+  const { stdout, stderr } = exec(`cd ${_directory} && ${_command} && cd ..`);
+  if(stdout) return stdout;
+  return stderr
+}
 
 const sourcePath = process.cwd();
 const directoryList = [];
 const getDirectories = (_path) => {
-    return fs.readdirSync(_path, { withFileTypes: true })
-        .filter(dirent => dirent.isDirectory())
-        .map(directory => directory.name);
+  return fs
+    .readdirSync(_path, { withFileTypes: true })
+    .filter((item) => item.isDirectory())
+    .map((directory) => directory.name);
 };
 
 const isGitRepo = (_directory) => {
-    const isGitRepo = spawnSync('git rev-parse --is-inside-work-tree', {encoding: 'utf8'});
-    console.log(isGitRepo);
-    return isGitRepo;
+  let flag = true;
+  try {
+    fs.accessSync(path.join(_directory, '/.git'), fs.constants.F_OK);
+  } catch (error) {
+    flag = false;
+  }
+  return flag;
 };
 
 getDirectories(sourcePath).forEach((directory) => {
-    //execSync(`cd ${directory}`, {encoding: 'utf8'});
-    spawnSync(`cd ${directory}`, {encoding: 'utf8'});
-    if (isGitRepo(directory)) directoryList.push({Directory: directory, Status: isGitRepo(directory)})
-    //spawnSync(['/bin/sh', 'script.sh']);
-    //console.log(isGitRepo(directory));
-    spawnSync('cd ..', {encoding: 'utf8'});
+  if (isGitRepo(directory)) {
+    const currentStatus = doGit(directory, 'git status');
+    directoryList.push({ Directory: directory, Status: currentStatus });
+  }
 });
 
-console.table(directoryList)
+console.table(directoryList);
+//doGit('xcro_odp', 'git pull');
